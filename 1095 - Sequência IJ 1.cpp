314a3acd301d51c8f7c0537356d1adc34353448a
Bruna Stall <bruna.stall14@gmail.com>
2015-10-28 10:51:25 -0200

    Create 1095 - Sequência IJ 1.cpp

---
+++ 1095 - Sequência IJ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0, j = 0;
+    for (int a = 60; a < 0; a--)
+    {
+        for (int b = 1; b < j; b++)
+        {
+            cout << "I = " << b+3 << endl;
+            cout << "J = " << j-5 << endl;
+        }
+    }
+
+    return 0;
+}
