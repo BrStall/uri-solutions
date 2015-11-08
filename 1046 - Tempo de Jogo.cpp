bf5dd58159c063e86292bc45f3f520e2696ed625
Bruna Stall <bruna.stall14@gmail.com>
2015-11-08 20:17:23 -0200

    Create 1046 - Tempo de Jogo.cpp

---
+++ 1046 - Tempo de Jogo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a >= b) cout << "O JOGO DUROU " << b + (24-a) << " HORA(S)" << endl;
+    else cout << "O JOGO DUROU " << b-a << " HORA(S)" << endl;
+}
